b24281f99307cb7da0d7634849f2697ad65cec66
mari0vs <95940743+mari0vs@users.noreply.github.com>
2022-11-14 23:21:53 -0600

    Init Listeners set

--- src/battleship/Init.java
+++ src/battleship/Init.java
@@ -1,9 +1,31 @@
 package battleship;
 
-public class Init {
-    public static void main (String[] args)
-    {
-    	MenuInterface mi = new MenuInterface();
-    	mi.setVisible(true);
-    }
+//import java.awt.*;
+import java.awt.event.*;
+
+public class Init implements ActionListener, MouseListener
+{
+	public static void main (String[] args)
+	{
+		MenuInterface mi = new MenuInterface();
+		mi.setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e)
+	{}
+
+	public void mouseClicked(MouseEvent e)
+	{}
+
+	public void mouseEntered(MouseEvent e)
+	{}
+
+	public void mouseExited(MouseEvent e)
+	{}
+
+	public void mousePressed(MouseEvent e)
+	{}
+
+	public void mouseReleased(MouseEvent e)
+	{}
 }
