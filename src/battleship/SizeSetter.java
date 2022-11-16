29e80da449bd35ab84132e91de4fb104534ece1d
mari0vs <95940743+mari0vs@users.noreply.github.com>
2022-11-16 11:06:12 -0600

    Update SizeSetter.java

--- src/battleship/SizeSetter.java
+++ src/battleship/SizeSetter.java
@@ -21,56 +21,56 @@ public class SizeSetter
 
 		if ((screenWidth/screenHeight) > 1.5)
 		{
-			sizes[0] = (int) Math.round(screenHeight*0.3);
-			sizes[1] = (int) Math.round(screenWidth*0.2);
+			sizes[0] = (int) Math.round(screenHeight*0.3);// h next turn panel
+			sizes[1] = (int) Math.round(screenWidth*0.2);// w next turn panel
 
-			sizes[2] = (int) Math.round(screenHeight*0.05);
-			sizes[3] = (int) Math.round(screenWidth*0.05);
+			sizes[2] = (int) Math.round(screenHeight*0.05);// h next turn button
+			sizes[3] = (int) Math.round(screenWidth*0.05);// w next turn button
 
-			sizes[4] = (int) Math.round(screenWidth*0.04);
+			sizes[4] = (int) Math.round(screenWidth*0.04);// w shooting
 
-			sizes[5] = (int) Math.round(screenWidth*0.2);
+			sizes[5] = (int) Math.round(screenWidth*0.2);// w player ships panel
 
-			sizes[6] = (int) Math.round(screenHeight*0.53);
+			sizes[6] = (int) Math.round(screenHeight*0.53);// h player ships panel
 
-			sizes[7] = (int) Math.round(screenWidth*0.04);
+			sizes[7] = (int) Math.round(screenWidth*0.04);// w ship placement padding
 
-			sizes[8] = (int) Math.round(screenWidth*0.18);
+			sizes[8] = (int) Math.round(screenWidth*0.18);// w selection panel
 
-			sizes[9] = (int) Math.round(screenHeight*0.35);
+			sizes[9] = (int) Math.round(screenHeight*0.35);// h done panel
 
-			sizes[10] = (int) Math.round(screenWidth*0.4);
+			sizes[10] = (int) Math.round(screenWidth*0.4);// w winner player panels
 
-			sizes[11] = (int) Math.round(screenHeight*0.1);
+			sizes[11] = (int) Math.round(screenHeight*0.1);// h winner player panels
 
-			sizes[12] = (int) Math.round(screenHeight*0.35);
+			sizes[12] = (int) Math.round(screenHeight*0.35);// h winner main menu panel
 		}
 
 		else
 		{
-			sizes[0] = (int) Math.round(screenHeight*0.2);
-			sizes[1] = (int) Math.round(screenWidth*0.1);
+			sizes[0] = (int) Math.round(screenHeight*0.2);// h next turn panel
+			sizes[1] = (int) Math.round(screenWidth*0.1);// w next turn panel
 
-			sizes[2] = (int) Math.round(screenHeight*0.05);
-			sizes[3] = (int) Math.round(screenWidth*0.05);
+			sizes[2] = (int) Math.round(screenHeight*0.05);// h next turn button
+			sizes[3] = (int) Math.round(screenWidth*0.05);// w next turn button
 
-			sizes[4] = (int) Math.round(screenWidth*0.04);
+			sizes[4] = (int) Math.round(screenWidth*0.04);// w shooting
 
-			sizes[5] = (int) Math.round(screenWidth*0.2);
+			sizes[5] = (int) Math.round(screenWidth*0.2);// w player ships panel
 
-			sizes[6] = (int) Math.round(screenHeight*0.53);
+			sizes[6] = (int) Math.round(screenHeight*0.53);// h player ships panel
 
-			sizes[7] = (int) Math.round(screenWidth*0.04);
+			sizes[7] = (int) Math.round(screenWidth*0.04);// w ship placement padding
 
-			sizes[8] = (int) Math.round(screenWidth*0.18);
+			sizes[8] = (int) Math.round(screenWidth*0.18);// w selection panel
 
-			sizes[9] = (int) Math.round(screenHeight*0.35);
+			sizes[9] = (int) Math.round(screenHeight*0.35);// h done panel
 
-			sizes[10] = (int) Math.round(screenWidth*0.4);
+			sizes[10] = (int) Math.round(screenWidth*0.4);// w winner player panels
 
-			sizes[11] = (int) Math.round(screenHeight*0.1);
+			sizes[11] = (int) Math.round(screenHeight*0.1);// h winner player panels
 
-			sizes[12] = (int) Math.round(screenHeight*0.35);
+			sizes[12] = (int) Math.round(screenHeight*0.35);// h winner main menu panel
 		}
 
 		print();
